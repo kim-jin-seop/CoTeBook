b34863cf985306478e25b39e48428d77898426e9
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-07-18 18:46:08 +0900

    solve problem

--- src/level1_problem_12912/Solution.java
+++ src/level1_problem_12912/Solution.java
@@ -3,22 +3,24 @@ package level1_problem_12912;
 public class Solution {
     public long solution(int a, int b) {
         long answer = 0;
-        boolean a_IsBig = Math.abs(a) > Math.abs(b) ? true : false;
+        long a_abs = Math.abs(a);
+        long b_abs = Math.abs(b);
+        boolean a_IsBig = a_abs > b_abs ? true : false;
         boolean a_IsPlus = a >= 0 ? true : false;
         boolean b_IsPlus = b >= 0 ? true : false;
 
-        long a_Sum = Math.abs(a) * Math.abs(a + 1) / 2;
-        long b_Sum = Math.abs(b) * Math.abs(b + 1) / 2;
+        long a_Sum = a_abs * (a_abs+1) / 2;
+        long b_Sum = b_abs * (b_abs+1) / 2;
 
         if(a_IsBig) {
             answer = a_Sum - b_Sum;
             if(!a_IsPlus)
-                answer = answer*(-1);
+                answer *= -1;
         }
         else {
             answer = b_Sum - a_Sum;
             if(!b_IsPlus)
-                answer = answer*(-1);
+                answer *= -1;
         }
 
         if((a_IsPlus && b_IsPlus)||(!a_IsPlus&&!b_IsPlus)){
