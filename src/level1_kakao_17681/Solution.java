50e2f2a5965dd543298666616c2fec77315463ea
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-07-19 19:23:11 +0900

    Create Solution.java

---
+++ src/level1_kakao_17681/Solution.java
@@ -0,0 +1,10 @@
+package level1_kakao_17681;
+
+public class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+
+        
+        return answer;
+    }
+}
