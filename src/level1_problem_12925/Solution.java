4725aa2e91c563504ec182cc2bc026f1b5cd6600
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-07-25 18:09:45 +0900

    문자열 -> 정수(- 부호도 있음)
    - 부호가 있어도 parseInt를 사용하면 -도 처리함

---
+++ src/level1_problem_12925/Solution.java
@@ -0,0 +1,9 @@
+package level1_problem_12925;
+
+public class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
