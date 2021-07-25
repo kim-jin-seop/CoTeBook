8c38b8ae1a354aa583233379db19e9270be6c0eb
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-07-25 18:35:00 +0900

    약수의 합
    약수 더하기(나머지 연산)

---
+++ src/level1_problem_12928/Solution.java
@@ -0,0 +1,12 @@
+package level1_problem_12928;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i = 1; i <= n ; i++){
+            if(n%i == 0)
+                answer += i;
+        }
+        return answer;
+    }
+}
