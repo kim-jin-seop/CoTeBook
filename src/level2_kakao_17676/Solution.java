9ac972b5aa6295b1a6ec3696e4614c1ca5c68a97
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-07-30 01:30:31 +0900

    update
    update

--- src/level2_kakao_17676/Solution.java
+++ src/level2_kakao_17676/Solution.java
@@ -1,18 +1,16 @@
 package level2_kakao_17676;
 
+import java.sql.Time;
+
 public class Solution {
+
+    public static void main(String args[]){
+        Time t = new Time(22);
+    }
     public int solution(String[] lines) {
-        int maxThroughput = 0, nowThroughput = 0;
+        String[][] worksTime = new String[lines.length][2];
 
-        for(int i = 0; i < lines.length; i++){
-            String[] splitData = lines[i].split(" ");
-            String[] serviceStartTime = splitData[1].split(":");
-            String processingTime = splitData[2].substring(0,splitData[2].length()-1);
-
-            String startTime_Hour = serviceStartTime[0];
-            String startTime_Min = serviceStartTime[1];
-            String startTime_Sec = serviceStartTime[2];
-
+        for (int i = 0; i < lines.length; i++) {
 
         }
         return 0;
