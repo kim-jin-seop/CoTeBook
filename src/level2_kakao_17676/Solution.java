0f40933b0105099e730c4ddec9c1222bc695794b
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-07-29 16:09:39 +0900

    Update Solution.java

--- src/level2_kakao_17676/Solution.java
+++ src/level2_kakao_17676/Solution.java
@@ -2,7 +2,19 @@ package level2_kakao_17676;
 
 public class Solution {
     public int solution(String[] lines) {
-        int answer = 0;
-        return answer;
+        int maxThroughput = 0, nowThroughput = 0;
+
+        for(int i = 0; i < lines.length; i++){
+            String[] splitData = lines[i].split(" ");
+            String[] serviceStartTime = splitData[1].split(":");
+            String processingTime = splitData[2].substring(0,splitData[2].length()-1);
+
+            String startTime_Hour = serviceStartTime[0];
+            String startTime_Min = serviceStartTime[1];
+            String startTime_Sec = serviceStartTime[2];
+
+
+        }
+        return 0;
     }
 }
