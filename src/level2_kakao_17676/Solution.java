fba25829f2b67554dca72357d1622b93308be2fa
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-07-31 19:36:35 +0900

    Update Solution.java

--- src/level2_kakao_17676/Solution.java
+++ src/level2_kakao_17676/Solution.java
@@ -1,6 +1,8 @@
 package level2_kakao_17676;
 
 import java.sql.Time;
+import java.time.*;
+import java.util.ArrayList;
 
 public class Solution {
 
@@ -9,10 +11,27 @@ public class Solution {
     }
     public int solution(String[] lines) {
         String[][] worksTime = new String[lines.length][2];
+        ArrayList<LocalDateTime> finishTimes = new ArrayList<>();
+        ArrayList<LocalDateTime> startTimes = new ArrayList<>();
 
         for (int i = 0; i < lines.length; i++) {
-
+            String[] splitline = lines[i].split(" ");
+            LocalDate date = LocalDate.parse(splitline[0]);
+            LocalTime time = LocalTime.parse(splitline[1]);
+            finishTimes.add(LocalDateTime.of(date,time.minusSeconds(-1)));
+            startTimes.add(LocalDateTime.of(date,time)
+                    .minusNanos(1000000000L*Long.parseLong(splitline[2].substring(0,splitline[2].length()-1)+0.01)));
         }
-        return 0;
+        int max = 0;
+        int count = 0;
+        for(int i = 0; i < finishTimes.size(); i++){
+            for(int j = i; j < startTimes.size(); j++){
+                if(finishTimes.get(i).isBefore(startTimes.get(j)))
+                    count++;
+            }
+            if(count > max)
+                max = count;
+        }
+        return max;
     }
 }
