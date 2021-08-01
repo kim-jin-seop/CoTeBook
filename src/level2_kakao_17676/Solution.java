8064c644783b3aeee8f71df7a28f02db8af083e0
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-08-01 19:05:41 +0900

    java8 Time pi 활용
    Java8의 새로운 기능인 LocalTime, LocalDate활용 시간계산 문제

--- src/level2_kakao_17676/Solution.java
+++ src/level2_kakao_17676/Solution.java
@@ -1,16 +1,12 @@
 package level2_kakao_17676;
 
-import java.sql.Time;
 import java.time.*;
 import java.util.ArrayList;
 
+
 public class Solution {
 
-    public static void main(String args[]){
-        Time t = new Time(22);
-    }
     public int solution(String[] lines) {
-        String[][] worksTime = new String[lines.length][2];
         ArrayList<LocalDateTime> finishTimes = new ArrayList<>();
         ArrayList<LocalDateTime> startTimes = new ArrayList<>();
 
@@ -18,18 +14,33 @@ public class Solution {
             String[] splitline = lines[i].split(" ");
             LocalDate date = LocalDate.parse(splitline[0]);
             LocalTime time = LocalTime.parse(splitline[1]);
-            finishTimes.add(LocalDateTime.of(date,time.minusSeconds(-1)));
-            startTimes.add(LocalDateTime.of(date,time)
-                    .minusNanos(1000000000L*Long.parseLong(splitline[2].substring(0,splitline[2].length()-1)+0.01)));
+            finishTimes.add(LocalDateTime.of(date, time).minusSeconds(-1).minusNanos(1000000 * 1));
+
+            String[] seconds = splitline[2].split("[.s]");
+            if (seconds.length == 2)
+                startTimes.add(LocalDateTime.of(date, time)
+                        .minusSeconds(Integer.parseInt(seconds[0]))
+                        .minusNanos(1000000 * Integer.parseInt(seconds[1]))
+                        .minusNanos(1000000 * -1));
+            else
+                startTimes.add(LocalDateTime.of(date, time)
+                        .minusSeconds(Integer.parseInt(seconds[0]))
+                        .minusNanos(1000000 * -1));
         }
+
+        int max = findMax(finishTimes, startTimes);
+        return max;
+    }
+
+    private int findMax(ArrayList<LocalDateTime> finishTimes, ArrayList<LocalDateTime> startTimes) {
         int max = 0;
-        int count = 0;
-        for(int i = 0; i < finishTimes.size(); i++){
-            for(int j = i; j < startTimes.size(); j++){
-                if(finishTimes.get(i).isBefore(startTimes.get(j)))
+        for (int i = 0; i < finishTimes.size(); i++) {
+            int count = 0;
+            for (int j = i; j < startTimes.size(); j++) {
+                if (!startTimes.get(j).isAfter(finishTimes.get(i)))
                     count++;
             }
-            if(count > max)
+            if (count > max)
                 max = count;
         }
         return max;
