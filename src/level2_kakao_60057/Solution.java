a3cd0d2a90f137a1aa1519586cbce0e0f7676a59
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-08-04 23:24:25 +0900

    add solution

--- src/level2_kakao_60057/Solution.java
+++ src/level2_kakao_60057/Solution.java
@@ -2,7 +2,33 @@ package level2_kakao_60057;
 
 public class Solution {
     public int solution(String s) {
-        int answer = 0;
-        return answer;
+        int min = s.length();
+        for (int count = 1; count <= s.length() / 2; count++) {
+            int length = findLength(s, count);
+            if (min > length)
+                min = length;
+        }
+        return min;
+    }
+
+    private int findLength(String s, int count) {
+        int length = 0;
+        String before = s.substring(0, count);
+        boolean isFirst = true;
+        for (int i = count; i < s.length(); i += count) {
+            if(i+count > s.length())
+                return length + (s.length()-i);
+            if (isFirst)
+                length += count;
+            if (!before.equals(s.substring(i, i + count))) {
+                before = s.substring(i, i + count);
+                isFirst = true;
+            } else {
+                if (isFirst)
+                    length += 1;
+                isFirst = false;
+            }
+        }
+        return length;
     }
 }
