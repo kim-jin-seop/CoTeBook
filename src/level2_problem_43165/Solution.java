032e2bd2824d5cdac3f6d131c4fdf8efaaed6fa3
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-08-08 20:50:58 +0900

    make number
    dfs 사용하기 위해 number class 선언

--- src/level2_problem_43165/Solution.java
+++ src/level2_problem_43165/Solution.java
@@ -1,4 +1,33 @@
 package level2_problem_43165;
 
 public class Solution {
+    public int solution(int[] numbers, int target) {
+        
+        return 0;
+    }
+
+    class number {
+        private int sign;
+        private int value;
+
+        public number(int value) {
+            this.value = value;
+        }
+
+        public int getSign() {
+            return sign;
+        }
+
+        public void setSign(int sign) {
+            this.sign = sign;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
 }
