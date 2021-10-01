213fd0d57858c8464b0d431be91f4d25d1f065c8
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-10-01 14:19:38 +0900

    프로젝트 이름 변경
    project이름 변경
    Programmers -> Cording Test

--- src/level2_problem_43165/Solution.java
+++ src/level2_problem_43165/Solution.java
@@ -1,33 +1,37 @@
 package level2_problem_43165;
 
+import java.util.Stack;
+
 public class Solution {
     public int solution(int[] numbers, int target) {
-        
+        Stack<Integer> stack = new Stack<>();
+        int count = 0;
+
+        //경우가 한개
+        int num = 0;
+        for(int i = 0; i < numbers.length; i++){
+            num += numbers[i];
+        }
+        if(num == target)
+            return 1;
+
+        for(int i = 0; i < numbers.length; i++){
+            boolean[] visit = new boolean[numbers.length];
+            visit[i] = true;
+            count += DFS(numbers,visit,stack);
+        }
+
+        return count;
+    }
+
+    private int DFS(int[] numbers, boolean[] visit, Stack<Integer> stack) {
+        int sum = 0;
+        for(int i = 0; i< numbers.length; i++){
+            if(visit[i])
+                sum -= numbers[i];
+        }
         return 0;
     }
 
-    class number {
-        private int sign;
-        private int value;
 
-        public number(int value) {
-            this.value = value;
-        }
-
-        public int getSign() {
-            return sign;
-        }
-
-        public void setSign(int sign) {
-            this.sign = sign;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public void setValue(int value) {
-            this.value = value;
-        }
-    }
 }
