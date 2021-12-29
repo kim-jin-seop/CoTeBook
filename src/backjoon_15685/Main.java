bc386593d1db1997464b268166808e1f98a12a61
JinSeopKim <33629459+kim-jin-seop@users.noreply.github.com>
2021-12-29 19:43:09 +0900

    드래곤 커브

--- src/backjoon_15685/Main.java
+++ src/backjoon_15685/Main.java
@@ -16,65 +16,62 @@ public class Main {
             int y = sc.nextInt();
             int d = sc.nextInt();
             int g = sc.nextInt();
-            makeCurve(location, x, y, d, g);
+            ArrayList<Integer> directions = new ArrayList<>();
+            directions.add(d);
+            if(g != 0)
+                findDirection(g, directions);
+            makeCurve(location, x, y, directions);
         }
 
-        int count = 0;
-        for(int i = 0; i < 101; i++){
-            for(int j = 0; j < 101; j++){
-            }
-        }
-
+        int count = countingSquare(location);
         System.out.println(count);
     }
 
-    public static void makeCurve(boolean[][] location, int x, int y, int d, int g) {
-        ArrayList<int[]> list = new ArrayList<>();
-        addCurvePoint(location, x, y, list);
-        System.out.println(x+" " + y);
-        // 0세대
-        if (d == 0)
-            x++;
-        else if (d == 1)
-            y--;
-        else if (d == 2)
-            x--;
-        else
-            y++;
-        addCurvePoint(location, x, y, list);
-        System.out.println(x+" " + y);
-        //1세대 이상
-        for (int i = 0; i < g; i++) {
-            int[] point = new int[2];
-            int size = list.size()-1;
-            boolean first = true;
-            for(int idx = 0; idx < size; idx++){
-                int[] loc = list.get(idx);
-                point = move(x, y, loc);
-                addCurvePoint(location,point[0],point[1],list);
-                if(first){
-                    x = point[0];
-                    y = point[1];
-                    first = false;
-                }
-                System.out.println(point[0] + " " + point[1]);
+    private static void makeCurve(boolean[][] location, int x, int y, ArrayList<Integer> directions) {
+        //첫 점
+        location[x][y] = true;
+        for (int i = 0; i < directions.size(); i++) {
+            int direction = directions.get(i);
+            if (direction == 0) {
+                x += 1;
+            } else if (direction == 1) {
+                y -= 1;
+            } else if (direction == 2) {
+                x -= 1;
+            } else {
+                y += 1;
             }
-            first = true;
+            if (x >= 0 && y >= 0 && x < location.length && y < location.length) {
+                location[x][y] = true;
+            } else
+                break;
         }
     }
 
-    private static int[] move(int x, int y , int[] loc){
-        int locX = loc[0] - x;
-        int locY = loc[1] - y;
-        int[] mul = locX <= 0 && locY >= 0 ? new int[]{-1,1} : locX >=0 && locY >=0 ? new int[]{1,-1}
-                : locX >= 0 && locY <= 0 ? new int[]{-1,1} : new int[]{1,-1};
-        return new int[]{locY*mul[0]+x,locX*mul[1]+y};
+    private static void findDirection(int g, ArrayList<Integer> directions) {
+        for (int i = directions.size() - 1; i >= 0; i--) {
+            directions.add((directions.get(i) + 1) % 4);
+        }
+        if (g > 1)
+            findDirection(g - 1, directions);
     }
 
-    private static void addCurvePoint(boolean[][] location, int x, int y, ArrayList<int[]> list) {
-        if (x >= 0 && y >= 0 && x <= 100 && y <= 100) {
-            location[x][y] = true;
+
+    private static int countingSquare(boolean[][] location) {
+        int[] dx = {0, 0, 1, 1};
+        int[] dy = {0, 1, 0, 1};
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                int k = 0;
+                for (; k < 4; k++) {
+                    if (!location[i + dx[k]][j + dy[k]])
+                        break;
+                }
+                if (k == 4)
+                    count++;
+            }
         }
-        list.add(new int[]{x, y});
+        return count;
     }
 }
