2ea91b6041e87674b5a972715c6fb28681fae847
kim-jin-seop <tjq2702@naver.com>
2022-01-27 01:02:12 +0900

    멀쩡한 삼각형

--- programmers/pgm_62048/Solution.java
+++ programmers/pgm_62048/Solution.java
@@ -1,4 +1,18 @@
 package pgm_62048;
 
-public class Solution {
+class Solution {
+    public long solution(int w, int h) {
+        long answer = 1;
+        int gcd = gcd(w, h);
+        long remove = ((w / gcd) + (h / gcd) - 1) * gcd;
+        answer = ((long)w * (long)h) - remove;
+        return answer;
+    }
+
+    public int gcd(int a, int b) {
+        if (a % b == 0)
+            return b;
+        else
+            return gcd(b, a % b);
+    }
 }
