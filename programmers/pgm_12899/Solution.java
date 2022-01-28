2fa193caf9e2614dfd9954f0768d73446f34cc28
kim-jin-seop <tjq2702@naver.com>
2022-01-29 01:20:59 +0900

    124 나라의 숫자

--- programmers/pgm_12899/Solution.java
+++ programmers/pgm_12899/Solution.java
@@ -10,10 +10,25 @@ class Solution {
             list.add(n%3);
             n = n / 3;
         }
-        
+
+        for(int i = 0; i < list.size()-1; i++){
+            if(list.get(i) == 0) {
+                list.remove(i);
+                list.add(i,4);
+                int carry = list.remove(i + 1) - 1 ;
+                list.add(i+1,carry);
+            }
+            if(list.get(i) < 0){
+                int removeI = list.remove(i);
+                list.add(i,3+removeI);
+                int carry = list.remove(i + 1) - 1 ;
+                list.add(i+1,carry);
+            }
+        }
 
         for(int i = 0; i < list.size(); i++){
-            answer = list.get(i) + answer;
+            if(list.get(i) != 0)
+                answer = list.get(i) + answer;
         }
         return answer;
     }
