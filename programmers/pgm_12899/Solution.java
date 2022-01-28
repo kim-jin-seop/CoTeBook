be7461b1b85dc79c8ee307dc065738245c3c97ea
kim-jin-seop <tjq2702@naver.com>
2022-01-28 18:12:56 +0900

    Create Solution.java

---
+++ programmers/pgm_12899/Solution.java
@@ -0,0 +1,20 @@
+package pgm_12899;
+
+import java.util.ArrayList;
+
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        ArrayList<Integer> list = new ArrayList<>();
+        while (n > 0){
+            list.add(n%3);
+            n = n / 3;
+        }
+
+
+        for(int i = 0; i < list.size(); i++){
+            answer = list.get(i) + answer;
+        }
+        return answer;
+    }
+}
