493527589a1a5996443840920615fad757edc9fc
kim-jin-seop <tjq2702@naver.com>
2022-02-07 01:01:35 +0900

    test

--- programmers/pgm_60058/Solution.java
+++ programmers/pgm_60058/Solution.java
@@ -1,8 +1,23 @@
 package pgm_60058;
 
+import java.util.Stack;
+
 public class Solution {
     public String solution(String p) {
         String answer = "";
+        if(p.length() == 0)
+            return "";
+
+        int checkpoint = 0;
+
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = checkpoint; i < p.length(); i++){
+            char bracket = p.charAt(i);
+        }
+
+
+
         return answer;
     }
 }
