eebcbcd3f15868977729a72aa851c2320563bc9e
kim-jin-seop <tjq2702@naver.com>
2022-02-23 22:37:44 +0900

    vuepress

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env sh
 
-# abort on errors
+# 오류 발생시 중단한다.
 set -e
 
-# build
+# 문서(md)를 build하여 html로 만든다.
 npm run docs:build
 
-# navigate into the build output directory
+# build가 output된 폴더로 이동한다.
 cd docs/.vuepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
+# init + add + commit을 해준 다음
 git init
 git add -A
-git commit -m 'deploy with vuepress'
+git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# https://<USERNAME>.github.io 에 배포하는 경우
+# git push -f https://github.com/<USERNAME>/<USERNAME>.github.io.git master
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/kim-jin-seop/codingTest.git master:gh-pages
+# https://<USERNAME>.github.io/<REPO> 에 배포하는 경우
+# git push -f https://github.com/<USERNAME>/<REPO>.git master:gh-pages
+
+# 필자의 경우 TIL repository에 배포하기 때문에 아래와 같이 작성했다.
+git push -f https://github.com/kim-jin-seop/CodingTest.git master:gh-pages
 
 cd -
