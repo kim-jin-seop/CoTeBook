ed1db2e5293131a65aa66969f53236e95f7ecd33
kim-jin-seop <tjq2702@naver.com>
2022-02-23 21:16:25 +0900

    --all.sh master

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env sh
 
-# 오류 발생시 중단한다.
+# abort on errors
 set -e
 
-# 문서(md)를 build하여 html로 만든다. 
-yarn docs:build
+git pull
+git add -A
+git commit -m "$1 $2 $3 --all.sh master"
+git push origin master
 
-# build가 output된 폴더로 이동한다. 
+# build
+npm run docs:build
+# navigate into the build output directory
 cd docs/.vuepress/dist
 
-# init + add + commit을 해준 다음
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy with vuepress'
 
-# https://<USERNAME>.github.io 에 배포하는 경우
-# git push -f https://github.com/<USERNAME>/<USERNAME>.github.io.git master
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# https://<USERNAME>.github.io/<REPO> 에 배포하는 경우
-# git push -f https://github.com/<USERNAME>/<REPO>.git master:gh-pages
-
-# 필자의 경우 TIL repository에 배포하기 때문에 아래와 같이 작성했다.
-git push -f https://github.com/kim-jin-seop/CodingTest.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:kim-jin-seop/codingTest.git master:gh-pages
 
 cd -
